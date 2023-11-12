59c3fe40c1fed85847962fe35491b15a177d0714
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-11-12 01:24:25 -0600

    Add personal page with team member information and contact details

---
+++ streamlit/pages/6_Personals.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.set_page_config(page_title="Personal Page", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items={
+    "About": "https://www.linkedin.com/in/zschmitz https://github.com/Zacharia-Schmitz"
+})
+
+def personals_page():
+    # Page Title
+    st.title("About Us")
+
+    st.markdown("""Two Air Force veterans who met during the CodeUp Technology Data Science Bootcamp...""")
+    
+    # Create two columns
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    col2.image("pages/images/zac.png", use_column_width=True, width=400)
+    col4.image("pages/images/click.png", use_column_width=True, width=400)
+
+    col2.markdown("<h3 style='text-align: center; color: white;'>Zacharia Schmitz</h3>", unsafe_allow_html=True)
+    col4.markdown("<h3 style='text-align: center; color: white;'>Joshua Click</h3>", unsafe_allow_html=True)
+
+    col2.markdown("""
+    <div style='text-align:center'>
+    <a href='mailto:schmitz.zacharia@gmail.com'>
+    <img src='https://img.shields.io/badge/Gmail-%23EA4335.svg?style=plastic&amp;logo=gmail&amp;logoColor=white' alt='Gmail' style='max-width: 100%;'></a> 
+    <a href='https://www.linkedin.com/in/zschmitz/' rel='nofollow'>
+    <img src='https://img.shields.io/badge/LinkedIn-%230A66C2.svg?style=plastic&amp;logo=linkedin&amp;logoColor=white' alt='LinkedIn' style='max-width: 100%;'></a> 
+    <a href='https://github.com/Zacharia-Schmitz'>
+    <img src='https://img.shields.io/badge/GitHub-%23181717.svg?style=plastic&amp;logo=github&amp;logoColor=white' alt='Github' style='max-width: 100%;'></a>
+    </div><br>
+    """, unsafe_allow_html=True)
+
+    col4.markdown("""
+    <div style='text-align:center'>
+    <a href='mailto:joshua.click25@gmail.com'>
+    <img src='https://img.shields.io/badge/Gmail-%23EA4335.svg?style=plastic&amp;logo=gmail&amp;logoColor=white' alt='Gmail' style='max-width: 100%;'></a> 
+    <a href='https://www.linkedin.com/in/joshua-r-click/' rel='nofollow'>
+    <img src='https://img.shields.io/badge/LinkedIn-%230A66C2.svg?style=plastic&amp;logo=linkedin&amp;logoColor=white' alt='LinkedIn' style='max-width: 100%;'></a> 
+    <a href='https://github.com/Joshua-Click'>
+    <img src='https://img.shields.io/badge/GitHub-%23181717.svg?style=plastic&amp;logo=github&amp;logoColor=white' alt='Github' style='max-width: 100%;'></a>
+    </div><br>
+    """, unsafe_allow_html=True)
+
+
+
+# Call the function to display the about page
+personals_page()
