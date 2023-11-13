56c07d8171a05c8daf8e3bb23d8c6e67384a5a7d
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-11-13 13:57:06 -0600

    Update images path

--- streamlit/Introduction.py
+++ streamlit/Introduction.py
@@ -14,10 +14,10 @@ st.set_page_config(page_title="Introduction", page_icon=None, layout="wide", ini
 # DEMO APP:
 # streamlit hello
 
-st.image('pages/images/wordcloud.png')
+st.image('wordcloud.png')
 
 st.markdown("""
-<h2 style="font-size:30px; text-align:center;">We developed this dynamic tool that empowers aspiring data analysts to navigate the job market with precision and confidence.</h2>
+<h2 style="font-size:27px; text-align:center;">We developed this dynamic tool that empowers aspiring data analysts to navigate the job market with precision and confidence.</h2>
 
 <h2 style="font-size:20px; text-align:center;">Our project uses 33,000 job postings that were scraped from Google and then processed with machine learning models to validate the results. We're not just sifting through text; we're decoding the jargon of job descriptions to help future data analysts on the hunt for their dream roles. We provide them with a competitive edge by revealing the skills, qualifications, and trends that matter most.</h2>
 """, unsafe_allow_html=True)
